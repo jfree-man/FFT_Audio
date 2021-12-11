f188a2fa9421befa27adb8ebacac5e7972dfcba9
Jennifer Freeman <jenncfreeman@gmail.com>
2021-12-10 22:50:57 -0500

    bad_alloc issues

--- main.cpp
+++ main.cpp
@@ -49,9 +49,9 @@ public:
         //header is 44 bytes
         header = s.substr(0, 44);
         cout << header << "\n";
-        int32_t data_size = 0;
-        int32_t padded_data_size = 0;
-        int32_t power = 1;
+        uint64_t data_size = 0;
+        uint64_t padded_data_size = 0;
+        uint64_t power = 1; //what happens if power is too large
 
         data_size = *(int32_t *)&header[40]; //size in bytes
         cout << data_size;
@@ -61,7 +61,7 @@ public:
         {
             data_size = data_size + 1;
         }
-        int32_t num_16bits = data_size / 2;
+        uint64_t num_16bits = data_size / 2;
         while (power < num_16bits) //what to do if num_16bits is too large
         {
             power *= 2;
@@ -75,29 +75,47 @@ public:
         cout << "char " << sizeof(char) << "\n";
         cout << "int16_t " << sizeof(int16_t) << "\n";
         input.seekg(44);
-        for (uint32_t i = 0; i < num_16bits; i++) //think this is reading in things right
+        for (uint64_t i = 0; i < num_16bits; i++) //think this is reading in things right
         {
 
             input.read((char *)&temp, sizeof(int16_t)); //possibly working
-            data.push_back((int16_t)temp);
+                                                        // data.push_back((int16_t)temp);
+            data.push_back((double)temp);
 
             //cout << "data: " << data[i] << "\n";
         }
 
+        //difference between power and audio size
+        padded_data_size = power - num_16bits;
+
+        cout << "padding size" << padded_data_size << "\n";
+        data.insert(data.end(), padded_data_size, 0); //data size is a power of 2, catch exceptions here?
+
         //close input file
         cout << data.size() << "\n";
+        cout << data.at(262140) << "\n";
 
         input.close();
     }
-    int16_t assign(const uint32_t &i)
+    // int16_t assign(const uint32_t &i) //dumb name, should rename
+    // {
+    //     return data.at(i);
+    // }
+    int32_t datasize()
     {
-        return data.at(i);
+        return data.size();
+    }
+
+    vector<complex<double>> &get_data()
+    {
+        return data; //is this bad, making a copy?
     }
 
 private:
     string header;
     //is it unsigned?
-    vector<int16_t> data;
+    //vector<int16_t> data;
+    vector<complex<double>> data;
     //might want to add, sampling rate, data size etc.
 };
 
@@ -106,69 +124,158 @@ class fft
 public:
     fft(wav_file &audio)
     {
-        cooley_tukey(audio);
+        x = cooley_tukey(audio.get_data());
     }
 
-    void cooley_tukey(wav_file &audio)
+    vector<complex<double>> cooley_tukey(const vector<complex<double>> &dat)
     {
-        vector<complex<double>> even;
-        vector<complex<double>> odd;
+        //vector<complex<double>> fft_vec;
         const double pi = acos(-1.0L);
-        //const complex<double> imag = 1.0i;
-
-        double N_2 = 166454 / 2; //do I need to -1, I don't think so
-        double N = 166454;
-
-        complex<double> even_dft = 0;
-        complex<double> odd_dft = 0;
-
-        for (double i = 0; i < N_2; i++)
+        //get data size from wav_file
+        //get data from wav_file
+        uint64_t N = dat.size();
+        if (N == 1)
         {
-            for (double j = 0; j < N_2; j++) //sum over N/2-1, to capture last value N/2-1
+            //do nothing
+        }
+        else
+        {
+
+            vector<complex<double>> even;
+            vector<complex<double>> odd;
+
+            //complex<double> even_dft = 0;
+            //complex<double> odd_dft = 0;
+
+            //const complex<double> imag = 1.0i;
+
+            double N_2 = N / 2;
+            even.reserve(N_2);
+            // odd.reserve(N_2);
+            cout << even.max_size() << "\n";
+            cout << odd.max_size() << "\n";
+            //do I need to -1, I don't think so
+            // cout << "N is " << N << "\n";
+            //double N = 166454;
+
+            for (uint64_t i = 0; i < N_2; i++)
             {
-
-                //polar(r,theta)
-                //m=j, k=i
-                even_dft = even_dft + ((double)audio.assign(2.0 * j) * polar(1.0, (-2.0 * pi * j * i) / (N_2)));
-                odd_dft = odd_dft + ((double)audio.assign((2.0 * j) + 1) * polar(1.0, (-2.0 * pi * j * (i + 1)) / (N_2)));
+                even.push_back(dat.at(2 * i));
+                odd.push_back(dat.at(2 * i + 1));
+                //cout << "i" << i << "\n";
             }
-            even.push_back(even_dft);
-            odd.push_back(odd_dft);
-            // cout << "even dft" << even_dft << "\n";
-            // cout << "odd dft" << odd_dft << "\n";
-            even_dft = 0;
-            odd_dft = 0;
+            cout << "N_2" << N_2 << "\n";
+            cooley_tukey(&even);
+            cooley_tukey(&odd);
+
+            // cout << "N is " << N << "\n";
+            //cout << "even is " << even.size() << "\n";
+            //cout << "odd is " << odd.size() << "\n";
+
+            complex<double> q = 0;
+            for (uint64_t k = 0; k < N_2; k++)
+            {
+                //old code here
+                // for (double j = 0; j < N_2; j++) //sum over N/2-1, to capture last value N/2-1
+                // {
+
+                //     //polar(r,theta)
+                //     //m=j, k=i
+                //     //cout << "even at " << j << "is" << even.at(j) << "\n";
+                //     // cout << "odd at " << j << "is" << odd.at(j) << "\n";
+
+                //     //old code here
+                //     //even_dft = even_dft + (even.at(j) * polar(1.0, (-2.0 * pi * j * k) / (N_2)));
+                //     //odd_dft = odd_dft + (odd.at(j) * polar(1.0, (-2.0 * pi * j * k) / (N_2)));
+                // }
+                //even.push_back(even_dft);
+                //odd.push_back(odd_dft);
+
+                //cout << "even_dft " << k << "is" << even_dft << "\n";
+                //cout << "odd_dft" << k << "is" << odd_dft << "\n";
+
+                //old code here
+                // q = polar(1.0, (-2.0 * pi * k) / (N)) * odd_dft;
+                // cout << even_dft + q << "\n";
+                // x.insert(x.begin() + k, even_dft + q);
+                // x.insert(x.begin() + (k + N_2), even_dft - q);
+
+                //new code here
+                q = polar(1.0, (-2.0 * pi * (double)k) / (N)) * odd[k];
+                //cout << even[k] + q << "\n";
+                dat[k] = even[k] + q;
+                dat[k + N_2] = even[k] - q;
+
+                //cout << "x at " << k << "is" << x.at(k) << "\n";
+                //cout << "x at " << k + N_2 << "is" << x.at(k + N_2) << "\n";
+                //cout << "capacity: " << x.capacity() << "\n";
+                // cout << "even dft" << even_dft << "\n";
+                // cout << "odd dft" << odd_dft << "\n";
+
+                //old code
+                // even_dft = 0;
+                // odd_dft = 0;
+                q = 0; //probably don't need to do this
+            }
+
+            cout << "end of fft \n";
         }
+        // cout << "size of x" << x.size() << "\n";
 
-        cout << "end of fft \n";
-        complex<double> q = 0;
+        // if (x.size() > 0)
+        // {
+        //     cout << "x at 0" << x.at(0) << "\n";
+        // }
 
-        for (double k = 0; k < N_2; k++)
-        {
-            q = polar(1.0, (-2.0 * pi * k) / (N)) * odd.at(k);
-            cout << even.at(k) + q << "\n";
-            x.insert(x.begin() + k, even.at(k) + q);
-            x.insert(x.begin() + (k + N_2), even.at(k) - q);
-
-            cout << "x at " << k << "is" << x.at(k) << "\n";
-            cout << "x at " << k + N_2 << "is" << x.at(k + N_2) << "\n";
-            cout << "capacity: " << x.capacity() << "\n";
-        }
-
-        cout << "end of fft \n";
+        return dat;
     }
 
-private:
-    vector<complex<double>> x;
+    void print()
+    {
+        for (uint64_t i = 0; i < x.size(); i++)
+        {
+            double current = abs(x.at(i));
+            cout << current << "\n";
+        }
+    }
+    void min_max()
+    {
+        double min_val = 2000000000;
+        double max_val = 0;
+        for (uint64_t i = 0; i < x.size(); i++)
+        {
+            double current = abs(x.at(i));
+            if (current < min_val)
+            {
+                min_val = abs(x.at(i));
+            }
+            else if (current > max_val)
+            {
+                max_val = abs(x.at(i));
+            }
+        }
+        cout << "max val" << max_val << "\n";
+        cout << "min val" << min_val << "\n";
+    }
+
+private
+    :
+    vector<complex<double>>
+        x;
 };
 
 int main()
 {
     try
     {
-        wav_file input("sine.wav");
+        wav_file input("800_sine.wav");
         //cout << input.header << "\n"; can;t access member function
         fft test(input);
+        cout << "end of algo \n";
+
+        test.min_max();
+
+        //frequncy resolution is sample freq/(num FFT values)
     }
     //catch others, error opening file...
     catch (const invalid_argument &e)
