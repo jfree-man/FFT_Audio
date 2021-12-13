47647db6caf5743c6c40925b5008db98e6010dc8
Jennifer Freeman <jenncfreeman@gmail.com>
2021-12-12 19:12:06 -0500

    clean up

--- Project_3.cpp
+++ Project_3.cpp
@@ -13,9 +13,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream> //string stream
-#include <cmath>   //is this the right math file
+#include <sstream>
+#include <cmath>
 #include <complex>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
 /**
@@ -51,23 +54,27 @@ public:
     void read_wav(const string &input_string)
     {
 
-        string s;
-        string wav_identifier;
-        string current;
-        uint64_t data_size = 0;
-        uint64_t padded_data_size = 0;
-        uint64_t power = 1; //what happens if power is too large
-        uint64_t num_16bits = 1;
+        string s;                        // String to store input.
+        string wav_identifier;           // String to store expected WAV identifier.
+        string current;                  // Current input value.
+        int16_t temp;                    // Temporary variable to store input values.
+        uint64_t data_section = 0;       // Size of data section in file.
+        uint64_t power = 1;              // Nearest power of 2 to data size.
+        uint64_t data_size = 1;          // Size of data in 2-bytes.
+        uint64_t max_data_size = 262144; // Maximum allowable data size (2^18).
 
+        // Open input file.
         ifstream input(input_string, ios::binary);
 
         if (!input.is_open())
         {
-            cout << "Error opening input file!";
+            throw ios_base::failure("Error opening input file! \n");
         };
 
+        // Seek to WAV identifier position.
         input.seekg(8);
 
+        // Get WAV identifier.
         for (uint32_t i = 0; i < 4; i++)
         {
             current = (char)input.get();
@@ -79,53 +86,52 @@ public:
             throw invalid_argument("Not a .WAV file.");
         }
 
+        // Read in WAV header (44 bytes).
         input.seekg(0);
         getline(input, s);
-
-        //header is 44 bytes
         header = s.substr(0, 44);
-        cout << header << "\n";
 
-        data_size = *(int32_t *)&header[40]; //size in bytes
-        cout << data_size;
+        // Get data section size.
+        data_section = *(int32_t *)&header[40];
 
-        //check if mod2, else there is a padding byte
-        if (data_size % 2 != 0)
+        // Ensure data section is a multiple of 2.
+        if (data_section % 2 != 0)
         {
-            data_size = data_size + 1;
+            data_section = data_section + 1;
         }
-        num_16bits = data_size / 2;
 
-        while (power < num_16bits) //what to do if num_16bits is too large
+        // Calculate size of data in 16 bits.
+        data_size = data_section / 2;
+
+        if (data_size > max_data_size)
+        {
+            throw invalid_argument("File size is larger than maximum allowable.");
+        }
+
+        // Compute next closest power of two to data size.
+        while (power <= data_size)
         {
             power *= 2;
         }
-        cout << data_size << "\n";
-        cout << num_16bits << "\n";
-        cout << power << "\n";
 
-        int16_t temp;
-        cout << "char " << sizeof(char) << "\n";
-        cout << "int16_t " << sizeof(int16_t) << "\n";
+        // Seek to data chunk.
         input.seekg(44);
-        for (uint64_t i = 0; i < num_16bits; i++) //think this is reading in things right
-        {
 
-            input.read((char *)&temp, sizeof(int16_t)); //possibly working
+        // Read in data.
+        for (uint64_t i = 0; i < data_size; i++)
+        {
+            input.read((char *)&temp, sizeof(int16_t));
             data.push_back((int16_t)temp);
         }
 
-        //difference between power and audio size
-        padded_data_size = power - num_16bits;
-
-        cout << "padding size" << padded_data_size << "\n";
+        // Resize data to include padding.
         data.resize(power);
 
         input.close();
     }
 
     /**
-     * @brief Size of data chunk in WAV file.
+     * @brief Size of data in WAV file.
      * 
      * @return uint64_t
      */
@@ -142,7 +148,17 @@ public:
     vector<complex<double>> get_data()
     {
         vector<complex<double>> new_vec(data.begin(), data.end());
-        return new_vec; //is this bad, making a copy?
+        return new_vec;
+    }
+
+    /**
+     * @brief Get the header object.
+     * 
+     * @return string 
+     */
+    string get_header()
+    {
+        return header;
     }
 
 private:
@@ -201,7 +217,7 @@ public:
 
         const double pi = acos(-1.0);
 
-        //get data from wav_file
+        // Get size of input vector.
         uint64_t N = dat.size();
         if (N == 1)
         {
@@ -209,94 +225,54 @@ public:
         }
         else
         {
-
+            // Divide N by 2.
             double N_2 = (double)N / 2;
-            // cout << "N_2" << N_2 << "\n";
+
+            // Initialize to store DFTs for even and odd indices.
             vector<complex<double>> even;
             vector<complex<double>> odd;
 
-            //do I need to reserve?
-            //even.reserve(N_2);
-            //odd.reserve(N_2);
-
+            // Separate input vector into even and odd indices.
             for (uint64_t i = 0; i < (uint64_t)N_2; i++)
             {
                 even.push_back(dat[2 * i]);
                 odd.push_back(dat[2 * i + 1]);
             }
 
-            //recursive
+            // Recursively compute even and odd vectors.
             cooley_tukey(even);
             cooley_tukey(odd);
 
             complex<double> q = 0;
+
             for (uint64_t k = 0; k < (uint64_t)N_2; k++)
-
             {
-
+                // Twiddle factor times odd values.
                 q = polar(1.0, (-2.0 * pi * (double)k) / (double)(N)) * odd[k];
+
+                // Update k-th DFT.
                 dat[k] = even[k] + q;
                 dat[k + (uint64_t)N_2] = even[k] - q;
             }
-
-            cout << "end of fft \n";
         }
 
         return dat;
     }
 
-    //might not need this function
-    void write_file()
+    /**
+     * @brief Simple low pass filter to remove freqencies above a threshold.
+     * 
+     * @param fft_values FFT data.
+     * @return vector<complex<double>> 
+     */
+    vector<complex<double>> low_pass(vector<complex<double>> &fft_values, uint32_t &threshold)
     {
-        FILE *file_ptr = NULL;
-
-        file_ptr = fopen("fft.txt", "w"); //open file for writing
-        if (file_ptr == NULL)
+        for (uint32_t i = threshold; i < (fft_values.size() - threshold); i++)
         {
-            perror("Cannot open writing file");
+            fft_values[i] = 0;
         }
 
-        for (uint64_t jj = 0; jj < x.size(); jj++) //write each row of chain to file
-        {
-            if (fprintf(file_ptr, "%f %f\n", real(x[jj]), imag(x[jj])) < 0)
-            {
-                perror("Error occurred");
-                fclose(file_ptr);
-            }
-        }
-
-        fclose(file_ptr);
-    }
-    //might not need this function
-    void print()
-    {
-        for (uint64_t i = 0; i < x.size(); i++)
-        {
-            double current = abs(x.at(i));
-            cout << current << "\n";
-        }
-    }
-    //might not need this function
-    void min_max()
-    {
-        double min_val = 2000000000;
-        double max_val = 0;
-        for (uint64_t i = 0; i < x.size(); i++)
-        {
-            double current = abs(x[i]);
-            if (current < min_val)
-            {
-                min_val = current;
-            }
-            else if (current > max_val)
-            {
-                max_val = current;
-            }
-
-            cout << current << "\n";
-        }
-        cout << "max val " << max_val << "\n";
-        cout << "min val " << min_val << "\n";
+        return fft_values;
     }
 
     /**
@@ -306,7 +282,7 @@ public:
      */
     vector<complex<double>> get_data()
     {
-        return x; //is this bad, making a copy?
+        return x;
     }
 
 private:
@@ -349,54 +325,71 @@ public:
     /**
      * @brief Inverse Cooley-Tukey radix-2 FFT.
      * 
-     * @param dft values to perform inverse FFT on.
+     * @param freq_domain frequency domain values to perform inverse FFT on.
      * @return vector<complex<double>> inverse FFT values.
      */
-    vector<complex<double>> inverse(vector<complex<double>> &dft)
+    vector<complex<double>> inverse(vector<complex<double>> &freq_domain)
     {
-        //complex conjugate
-        for (uint64_t i = 0; i < dft.size(); i++)
+        // Compute complex conjugate of data in freqency domain.
+        for (uint64_t i = 0; i < freq_domain.size(); i++)
         {
-            dft[i] = conj(dft[i]);
+            freq_domain[i] = conj(freq_domain[i]);
         }
 
-        //fft
-        fft out(dft);
+        // Compute forward FFT.
+        fft out(freq_domain);
+        freq_domain = out.get_data();
 
-        dft = out.get_data();
-        uint64_t N = dft.size();
+        uint64_t N = freq_domain.size();
 
-        //complex conjugate
-        //divide by N
-        for (uint64_t i = 0; i < dft.size(); i++)
+        // Compute complex conjugate and scale output by N.
+        for (uint64_t i = 0; i < freq_domain.size(); i++)
         {
-            dft[i] = conj(dft[i]) / (double)N;
+            freq_domain[i] = conj(freq_domain[i]) / (double)N;
         }
 
-        return dft;
+        return freq_domain;
     }
 
-    //not sure if I need this member function
-    void write_file()
+    /**
+     * @brief Write Inverse FFT values to WAV file.
+     * 
+     * @param header WAV file header.
+     */
+    int16_t write_wav(string &header)
     {
-        FILE *file_ptr = NULL;
+        stringstream ss_wav;
 
-        file_ptr = fopen("ifft.txt", "w"); //open file for writing
-        if (file_ptr == NULL)
+        // Create output file name.
+        chrono::system_clock now;
+        time_t output_time = chrono::system_clock::to_time_t(now.now());
+        ss_wav << put_time(localtime(&output_time), "%y%m%d_%H%M%S_.wav");
+
+        // Create output WAV file.
+        ofstream output_wav(ss_wav.str(), ios::binary);
+
+        if (!output_wav.is_open())
         {
-            perror("Cannot open writing file");
+            cout << "Error opening output file!";
+            return -1;
         }
 
-        for (uint64_t jj = 0; jj < y.size(); jj++) //write each row of chain to file
+        // Write WAV file header.
+        for (uint64_t i = 0; i < header.size(); i++)
         {
-            if (fprintf(file_ptr, "%f %f\n", real(y[jj]), imag(y[jj])) < 0)
-            {
-                perror("Error occurred");
-                fclose(file_ptr);
-            }
+            output_wav.write(&header[i], sizeof(int8_t));
         }
 
-        fclose(file_ptr);
+        // Write WAV data with real part of inverse FFT values.
+        for (uint64_t j = 0; j < y.size(); j++)
+        {
+            double real_part = real(y[j]);
+            int16_t round_real = (int16_t)round(real_part);
+            output_wav.write((char *)&round_real, sizeof(int16_t));
+        }
+
+        output_wav.close();
+        return 0;
     }
 
 private:
@@ -411,41 +404,131 @@ private:
     vector<complex<double>> y;
 };
 
-int main()
+/**
+ * @brief frequency class
+ * 
+ */
+class frequency
 {
-    try
+public:
+    // ===========
+    // Constructor
+    // ===========
+
+    /**
+     * @brief Construct a new frequency object.
+     * 
+     * @param num string to convert to frequency class.
+     */
+    frequency(const string &num)
     {
-        vector<complex<double>> input_data;
-        vector<complex<double>> output_data;
+        uint32_t input_freq = 0;
 
-        // Read and validate WAV file.
-        wav_file input("440_sine.wav");
+        // Verify input is an integer.
+        input_freq = stol(num);
 
-        cout << input.datasize();
-        input_data = input.get_data();
+        // Verify frequency is in audible range.
+        if (input_freq < 20 || input_freq > 20000)
+        {
+            throw out_of_range("Frequency outside audible range (20 Hz, 20000 Hz) \n");
+        }
 
-        // Compute fast fourier tranform on input WAV file.
-        fft test(input_data);
-        cout << "end of algo \n";
-
-        //test.min_max();
-        test.write_file();
-
-        // Compute inverse fast fourier transform on input transformed values.
-        output_data = test.get_data();
-        i_fft output(output_data);
-
-        output.write_file();
-    }
-    //catch others, error opening file...
-    catch (const invalid_argument &e)
-    {
-        cout << "Error: " << e.what() << '\n';
+        f = input_freq;
     }
 
-    //To Do
-    //1. Check exceptions fo all functions being used
-    //2. Use index instead of .at() where makes sense
-    //3. Restrict input file to be of certain size, or split it
-    //4. Consistenct with FFt, fft, include DFT
+    // =======================
+    // Public member functions
+    // =======================
+
+    /**
+     * @brief Get the data object.
+     * 
+     * @return uint32_t frequency.
+     */
+    uint32_t get_data()
+    {
+        return f;
+    }
+
+private:
+    // ============
+    // Private data
+    // ============
+
+    /**
+     * @brief frequency in Hz.
+     * 
+     */
+    uint32_t f;
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc == 4)
+    {
+        try
+        {
+            vector<complex<double>> input_data;
+            vector<complex<double>> output_data;
+            vector<complex<double>> freq_data;
+            string arg_two;
+
+            // Read and validate WAV file.
+            wav_file input(argv[1]);
+
+            input_data = input.get_data();
+
+            // Compute FFT on input WAV file.
+            fft dft(input_data);
+            freq_data = dft.get_data();
+
+            arg_two = argv[2];
+            if (arg_two != "low")
+            {
+                cout << "Invalid argument for filter. Valid arguments are: \"low\" \n";
+                return -1;
+            }
+
+            frequency input_freq(argv[3]);
+            uint32_t freq = input_freq.get_data();
+
+            // Compute low pass filter on frequency domain
+            output_data = dft.low_pass(freq_data, freq);
+
+            // Compute inverse FFT on input FFT values.
+            i_fft output(output_data);
+
+            // Write inverse FFT values to output WAV file.
+            string input_header = input.get_header();
+            int16_t write_status = output.write_wav(input_header);
+
+            if (write_status < 0)
+            {
+                return -1;
+            }
+        }
+
+        catch (const invalid_argument &e)
+        {
+            cout << "Error: " << e.what() << '\n';
+            return -1;
+        }
+        catch (const out_of_range &e)
+        {
+            cout << "Error: " << e.what() << '\n';
+            return -1;
+        }
+        catch (const ios_base::failure &e)
+        {
+            cout << "Error: " << e.what() << '\n';
+            return -1;
+        }
+    }
+    else
+    {
+        cout << (argc - 1) << " argument(s) provided to program. Program requires three arguments: \n";
+        cout << "1. The name of a WAV audio file: \"filename.wav\" \n";
+        cout << "2. Digital filter to apply: \"low\" \n";
+        cout << "3. Frequency (Hz) to apply filter with: ex. 800 \n";
+    }
 }
